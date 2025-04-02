cf24369ef7a5f36290294733cf2b415d4e59cdcc
bryan.foucart97@gmail.com <bryan.foucart97@gmail.com>
2025-04-02 16:06:40 +0200

    server.js

--- front/public/assets/js/main.js
+++ front/public/assets/js/main.js
@@ -1,5 +1,6 @@
 import "./header/header.js";
 import "./dark-mode/dark-mode.js";
+import "./global/global.js";
 import "./scroll-to-top/scroll-to-top.js";
 import "./reseaux/reseaux.js";
 // import "./cryptos/cryptos.js";
--- front/server.js
+++ front/server.js
@@ -1,8 +1,17 @@
+import { fetchData } from "./public/assets/js/lib/functions.js";
 const express = require("express");
 const path = require("path");
 const app = express();
+require("dotenv").config();
 
 app.set("view engine", "ejs");
+
+const images = fetchData({
+  route:
+    "/games?key=db76511d21a143668bd090896650561f&dates=2019-09-01,2019-09-30&platforms=18,1,7",
+}).then((data) => {
+  return data.results;
+});
 
 app.use(express.static(path.join(__dirname, "public"))); // => /front/public/
 
@@ -12,6 +21,17 @@ app.get("/", (req, res) => {
 
 app.get("/contact", (req, res) => {
   res.render("pages/contact");
+});
+
+app.get("/masonry", async (req, res) => {
+  try {
+    const imagesData = await images;
+    console.log(imagesData);
+    res.render("pages/masonry", { images: imagesData });
+  } catch (err) {
+    console.log(err);
+    res.render("pages/masonry", { images: [] });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
