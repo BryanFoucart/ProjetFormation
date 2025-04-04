12ee0e6ec50965a460b61c0aad8389f7ec61a96c
bryan.foucart97@gmail.com <bryan.foucart97@gmail.com>
2025-04-04 11:56:38 +0200

    Initial Commit

--- front/public/assets/js/main.js
+++ front/public/assets/js/main.js
@@ -3,6 +3,4 @@ import "./dark-mode/dark-mode.js";
 import "./global/global.js";
 import "./scroll-to-top/scroll-to-top.js";
 import "./reseaux/reseaux.js";
-// import "./cryptos/cryptos.js";
-// import "./carrousel/carrousel.js";
 import "./lib/functions.js";
