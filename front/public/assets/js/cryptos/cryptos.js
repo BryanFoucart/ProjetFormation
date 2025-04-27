0061a9df60d6eae42b27d003d465ddf1d913c3a8
bryan.foucart97@gmail.com <bryan.foucart97@gmail.com>
2025-04-27 18:21:21 +0200

    27-04-25 18:21

--- front/public/assets/js/cryptos/cryptos.js
+++ front/public/assets/js/cryptos/cryptos.js
@@ -34,7 +34,39 @@ function create(loading, tabGames) {
       // for (let i = 0; i < tabGames.length; i += 2) {
       // let pair = tabGames.slice(i, i + 2);
       const divArea = document.createElement("div");
+      divArea.style.opacity = "0.7";
+      divArea.style.transform = "scale(0.7)";
       container.appendChild(divArea);
+      const anim = [
+        { opacity: "0.7", transform: "scale(0.7)" },
+        { opacity: "1", transform: "scale(1)" },
+      ];
+
+      const options = {
+        duration: 800,
+        easing: "ease-in-out",
+        fill: "forwards",
+      };
+
+      let lastScrollY = window.scrollY;
+      const observer = new IntersectionObserver(
+        (entries) => {
+          // Détection du sens du scroll
+          const currentScrollY = window.scrollY;
+          const isScrollingDown = currentScrollY > lastScrollY; //Permet de faire que l'anim se fait uniquement en descendant
+          lastScrollY = currentScrollY;
+          entries.forEach((entry) => {
+            if (entry.isIntersecting && isScrollingDown) {
+              entry.target.animate(anim, options);
+              // observer.unobserve(entry.target); observer une seule fois
+            }
+          });
+        },
+        {
+          threshold: 0.5,
+        }
+      );
+
       // pair.map((game) => {
       const card = document.createElement("div");
       card.setAttribute("class", "game-card");
@@ -54,6 +86,7 @@ function create(loading, tabGames) {
       headerCard.append(nameGame, dateGame);
       card.append(headerCard, imgGame, avatarGame);
       divArea.append(card);
+      observer.observe(divArea);
       // });
       // console.log(pair);
     });
